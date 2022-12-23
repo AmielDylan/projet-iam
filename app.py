c5def4e7065310f7da2135124ce34309b24d118c
AmielDylan <amieladjovi@yahoo.fr>
2022-12-23 04:29:00 +0100

    commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/", methods =["GET", "POST"])
 def home():
     # Resultat de la requete
-    result = []
+    result = class_list_1 = class_list_2 = []
 
     if request.method == "POST":
         # getting input with name = med-1 in HTML form
@@ -19,7 +19,29 @@ def home():
             med_1=med_1,
             med_2=med_2)
 
-    return render_template('index.html', resultat = result)
+    return render_template('index.html', resultat = result, class_list_1 = class_list_1, class_list_2 = class_list_2)
+
+@app.route('/testClasse', methods=['POST'])
+def testClasse():
+    medicament = request.form.get("medTest")
+    return isClasse(medicament=medicament)
+
+@app.route('/testSubstance', methods=['POST'])
+def testSubstance():
+    medicament = request.form.get("medTest")
+    return isSubstance(medicament=medicament)
+
+@app.route('/getListClasses', methods=['POST'])
+def getListClasses():
+    resultat = []
+    substance = request.form.get("substance")
+    listId = getClassesId(substance=substance)
+
+    for value in listId:
+        resultat.append(getClasse(value))
+
+    return resultat
+
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -1,13 +1,91 @@
-window.onload = function(){
-    var med_1 = document.getElementById("choix-classe-1");
-    var med_2 = document.getElementById("choix-classe-2");
 
-    med_1.style.display = 'none'
-    med_2.style.display = 'none'
-}
-
-function dispose_result() {
+function dispose_result(){
     var element = document.getElementById("third");
     element.classList.toggle("invisible");
     element.style.display = 'none'
+}
+
+function clear_border(){
+  input_Med.classList.remove("danger")
+  input_Med.classList.remove("warning")
+  input_Med.classList.remove("valide")
+}
+
+async function testClasse(medicament){
+    var formdata = new FormData();
+    formdata.append("medTest", medicament);
+    
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+    
+    const response = await fetch("http://127.0.0.1:5000/testClasse", requestOptions)
+      .then(response => response.text());
+
+    return response
+}
+
+async function testSubstance(medicament){
+    var formdata = new FormData();
+    formdata.append("medTest", medicament);
+    
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+    
+    const response = await fetch("http://127.0.0.1:5000/testSubstance", requestOptions)
+      .then(response => response.text());
+
+    return response
+}
+
+async function getListClasses(substance){
+    var formdata = new FormData();
+    formdata.append("medTest", substance);
+    
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+    
+    const response = await fetch("http://127.0.0.1:5000/getListClasses", requestOptions)
+      .then(response => response.text());
+
+    return response
+}
+
+async function check_medicament(input_number){
+    var input_Med = document.getElementById("med-"+input_number);
+    var result = []
+    var testCls = await testClasse(input_Med.value)
+    var testSub = await testSubstance(input_Med.value)
+    
+    if(input_Med.value == "" || (testCls == "false" && testSub == "false")){
+        input_Med.classList.add("danger")
+        input_Med.classList.remove("warning")
+        input_Med.classList.remove("valide")
+
+        result = []
+    // }else{
+    //     if (input_Med.value == "SUB"){
+    //         input_Med.classList.remove("danger")
+    //         input_Med.classList.add("warning")
+    //         input_Med.classList.remove("valide")       
+
+    //         result.push("des classes")
+    //     }else{
+    //         input_Med.classList.remove("danger")
+    //         input_Med.classList.remove("warning")
+    //         input_Med.classList.add("valide") 
+
+    //         result.push("une classe")
+    //     }
+    }
+
+    console.log(result)
 }
